c381cfda2e557cb69058db15179b98db1b96b776
HAIYIN ZHU <31604927+zhyxyb@users.noreply.github.com>
2021-06-19 14:06:42 +0800

    js middleware sample

---
+++ index.js
@@ -0,0 +1,53 @@
+const fA = (ctx,next) => {
+    console.log('pre A start')
+    console.log(ctx)
+    console.log('pre a finish')
+    next(ctx)
+    console.log('after a start')
+    console.log(ctx)
+    console.log('after A finish')
+}
+
+const fB = (ctx,next) => {
+    console.log('pre B start')
+    console.log(ctx)
+    console.log('pre B finish')
+    next(ctx)
+    console.log('after B start')
+    console.log(ctx)
+    console.log('after B finish')
+}
+const fC = (ctx,next) => {
+    console.log('pre C start')
+    console.log(ctx)
+    console.log('pre C finish')
+    next(ctx)
+    console.log('after C start')
+    console.log(ctx)
+    console.log('after C finish')
+}
+
+const ff = (ctx) => {
+    console.log('this is ff')
+}
+
+let mds = [fA, fB, fC]
+let current=0
+
+function next(ctx) {
+    current++
+    if (current < mds.length) {
+
+        mds[current](ctx,next)
+
+    }
+    else {
+        ff(ctx)
+    }
+}
+function start(ctx) {
+     current = -1;
+    next(ctx)
+}
+
+start(5)
